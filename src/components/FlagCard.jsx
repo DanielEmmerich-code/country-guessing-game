5d7bdf0bb8d8a5e764700e8fe7b2cfce4ac8ddd3
DanielEmmerich <danielemmerich@outlook.de>
2026-03-16 20:20:29 +0100

    chore: new file for Flag Quiz and Flag Card

---
+++ src/components/FlagCard.jsx
@@ -0,0 +1,11 @@
+function FlagCard() {
+
+    return (
+        <div className="flag-card">
+            <div className="answers-card">
+
+            </div>
+        </div>
+    )
+}
+export default FlagCard;
